94a8ed88925cc6e534d54abf11953f6d516fe686
Amani <Amanikhunter@gmail.com>
2019-10-22 13:25:39 -0400

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Amani Hunter on 10/22/19.
+//  Copyright © 2019 Amani Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
